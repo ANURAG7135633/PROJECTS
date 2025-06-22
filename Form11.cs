c12970b88c31f2c8a58d776811f9f5a4adb0d853
ANURAG7135633 <anuragmohapatra121@gmail.com>
2025-06-22 11:16:02 +0530

    Add files via upload

---
+++ Form11.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace anuragwinapp
+{
+    public partial class Form11 : Form
+    {
+        private object textBoxName;
+        private object textBoxRoll;
+
+        public Form11()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string student = "Name: " + textBox1.Text +
+                             ", Roll: " + textBox2.Text +
+                             ", Mail: " + textBox3.Text +
+                             ", Phone: " + textBox4.Text +
+                             ", Gender: " + textBox5.Text +
+                             ", Address: " + textBox6.Text;
+                               listBox1.Items.Add(student);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex >= 0)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace anuragwinapp
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label27.Text = "ANURAG MOHAPATRA";
+            label8.Text = "→ DAV (70%)";
+            label9.Text = "→ Freedom School (75%)";
+            label10.Text = "→ ABIT CGPA(8.90) ";
+            label11.Text = "→ .NET";
+            label12.Text = "→ Python";
+            label13.Text = "→ SQL";
+            label14.Text = "→ 1st in music";
+            label15.Text = "→ 3rd in drawing";
+            label16.Text = "→ 3rd in group song";
+            label17.Text = "→ Student management system";
+            label18.Text = "→ Student slam book";
+            label19.Text = "→ Mines game ";
+            label20.Text = "→ singing";
+            label21.Text = "→ Drawing";
+            label22.Text = "→ Book reading";
+            label23.Text = "→  to learn";
+            label24.Text = "→ Open for any new skill adoption";
+            label28.Text = "6372897733";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
